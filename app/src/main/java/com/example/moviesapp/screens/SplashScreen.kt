0866611d16398ed7cc66f2e8c772b7107ec705d9
vlad513 <78737484+vlad513@users.noreply.github.com>
2024-02-25 00:30:16 +0300

    Изменил логотип приложения , SplashScreen.kt. Изменил название приложения

--- app/src/main/java/com/example/moviesapp/screens/SplashScreen.kt
+++ app/src/main/java/com/example/moviesapp/screens/SplashScreen.kt
@@ -2,17 +2,18 @@ package com.example.moviesapp.screens
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -45,13 +46,12 @@ fun Splash(alpha: Float) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Icon(
+        Image(
             modifier = Modifier
                 .size(120.dp)
                 .alpha(alpha = alpha),
-            imageVector = Icons.Default.PlayArrow,
-            contentDescription = "",
-            tint = Color.Black
+            imageVector =ImageVector.vectorResource(id = com.example.moviesapp.R.drawable.icons8_netflix_desktop_app) ,
+            contentDescription = ""
         )
     }
 }
