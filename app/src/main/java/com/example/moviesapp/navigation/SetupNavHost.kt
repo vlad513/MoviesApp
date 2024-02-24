3605d18531b51caee5023a696ec6ae234e413ead
vlad513 <78737484+vlad513@users.noreply.github.com>
2024-02-24 23:02:32 +0300

    Реализовал дизайн карточки itemMovies с подгрузкой изображения , создал экран DetailsScreen.kt и сделал на него пеход из экрана MainScreen.kt

--- app/src/main/java/com/example/moviesapp/navigation/SetupNavHost.kt
+++ app/src/main/java/com/example/moviesapp/navigation/SetupNavHost.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.moviesapp.MainViewModel
+import com.example.moviesapp.screens.DetailsScreen
 import com.example.moviesapp.screens.MainScreen
 import com.example.moviesapp.screens.SplashScreen
 import com.example.moviesapp.utils.Constant
@@ -17,19 +18,23 @@ sealed class Screens(val route: String) {
 }
 
 @Composable
-fun SetupNavHost(navController: NavHostController,viewModel: MainViewModel) {
+fun SetupNavHost(navController: NavHostController, viewModel: MainViewModel) {
     NavHost(
         navController = navController,
         startDestination = Screens.Splash.route
     ) {
-        composable(route = Screens.Splash.route){
+        composable(route = Screens.Splash.route) {
             SplashScreen(navController = navController, viewModel = viewModel)
         }
-        composable(route = Screens.Main.route){
+        composable(route = Screens.Main.route) {
             MainScreen(navController = navController, viewModel = viewModel)
         }
-        composable(route = Screens.Details.route){
-
+        composable(route = Screens.Details.route + "/{Id}") {backStackEntry ->
+            DetailsScreen(
+                navController = navController,
+                viewModel = viewModel,
+                itemId = backStackEntry.arguments?.getString("Id") ?: "1"
+            )
         }
     }
 }
